e2a734b6720384d10a15998ae6318c1b1877ad2e
Temur <ubaydullayev117@gmail.com>
2022-10-14 15:11:51 +0500

    Created AppDbContext

--- YouTube.WebApi/Data/DbContexts/AppDbContext.cs
+++ YouTube.WebApi/Data/DbContexts/AppDbContext.cs
@@ -1,7 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using YouTube.WebApi.Domain.Entities;
 
 namespace YouTube.WebApi.Data.DbContexts;
 
 public class AppDbContext : DbContext
 {
+    public virtual DbSet<User> Users { get; set; } = null!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(email => email.Email)
+            .IsUnique();
+    }
 }
