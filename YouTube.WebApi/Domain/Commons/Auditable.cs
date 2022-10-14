0fba0fa234d5aea9d38c076cd53d9477280c6e0c
Temur <ubaydullayev117@gmail.com>
2022-10-14 15:16:31 +0500

    Create Auditable class

--- YouTube.WebApi/Domain/Commons/Auditable.cs
+++ YouTube.WebApi/Domain/Commons/Auditable.cs
@@ -1,8 +1,17 @@
-﻿namespace YouTube.WebApi.Domain.Commons;
+﻿using System.Text.Json.Serialization;
+
+namespace YouTube.WebApi.Domain.Commons;
 
 public class Auditable
 {
     public uint Id { get; set; }
+
+    [JsonIgnore]
+    public bool IsActive { get; set; } = true;
+
+    [JsonIgnore]
     public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+
+    [JsonIgnore]
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
