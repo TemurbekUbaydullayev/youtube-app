4c6c1637fcd99c78ef07294e6a84480fd39e8c1d
Temur <ubaydullayev117@gmail.com>
2022-10-14 16:37:40 +0500

    UserRepository Interface added new method

---
+++ YouTube.WebApi/Data/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using YouTube.WebApi.Domain.Entities;
+
+namespace YouTube.WebApi.Data.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateAsync(User user);
+        Task<User> UpdateAsync(User user);
+        Task<User?> FindByEmailAsync(string email);
+        //Task ChangeActivityAsync(bool active, Expression<Func<User, bool>> expression);
+        Task<User?> GetAsync(Expression<Func<User, bool>> expression);
+        IQueryable<User> GetAll(Expression<Func<User, bool>>? expression = null, bool isTracking = true);
+    }
+}
