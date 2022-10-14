7b9306a1788a0c83603254231f13af5d49536d0a
Temur <ubaydullayev117@gmail.com>
2022-10-14 16:37:59 +0500

    update

---
+++ YouTube.WebApi/Data/Repositories/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using YouTube.WebApi.Data.DbContexts;
+using YouTube.WebApi.Data.Interfaces;
+using YouTube.WebApi.Domain.Entities;
+
+#pragma warning disable
+
+namespace YouTube.WebApi.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _dbSet;
+
+        public UserRepository(AppDbContext appDbContext)
+        {
+            _dbSet = appDbContext;
+        }
+        public async Task<User> CreateAsync(User user)
+        {
+            var entity = await _dbSet.Users.AddAsync(user);
+
+            return entity.Entity;
+        }
+
+        public async Task<User?> FindByEmailAsync(string email)
+        {
+            var entity = await _dbSet.Users.FirstOrDefaultAsync(email => email.Email.Equals(email));
+
+            return entity is null ? null : entity;
+        }
+
+        public IQueryable<User> GetAll(Expression<Func<User, bool>>? expression = null, bool isTracking = true)
+        {
+            var entities = expression is null ? _dbSet.Users : _dbSet.Users.Where(expression);
+
+            return isTracking ? entities : entities.AsNoTracking();
+        }
+
+        public async Task<User?> GetAsync(Expression<Func<User, bool>> expression)
+            => await _dbSet.Users.FindAsync(expression);
+
+        public async Task<User> UpdateAsync(User user)
+        {
+            var entity = _dbSet.Update(user);
+
+            return entity.Entity;
+        }
+    }
+}
