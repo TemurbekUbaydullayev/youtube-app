234e4cc09b7220cb6deabd9002a830887c81c22c
Temur <ubaydullayev117@gmail.com>
2022-10-15 19:16:32 +0500

    Created UserforCreationDto model

---
+++ YouTube.WebApi/Service/DTOs/Users/UserForViewDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YouTube.WebApi.Service.DTOs.Users
+{
+    public class UserForViewDto
+    {
+        public uint Id { get; set; }
+
+        [MinLength(2), MaxLength(50)]
+        public string FirstName { get; set; } = null!;
+
+        [MinLength(2), MaxLength(50)]
+        public string LastName { get; set; } = null!;
+
+        [MaxLength(70)]
+        public string Email { get; set; } = null!;
+
+        [MaxLength(200)]
+        public string ImageUrl { get; set; } = null!;
+    }
+}
