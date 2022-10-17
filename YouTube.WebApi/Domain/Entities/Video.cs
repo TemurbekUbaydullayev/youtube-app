8053b1c7a36fec254022491c468363b832665554
Temur <ubaydullayev117@gmail.com>
2022-10-17 08:29:47 +0500

    Video model added

---
+++ YouTube.WebApi/Domain/Entities/Video.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using YouTube.WebApi.Domain.Commons;
+
+namespace YouTube.WebApi.Domain.Entities;
+
+public class Video : Auditable
+{
+    [MinLength(1), MaxLength(50)]
+    public string Name { get; set; } = null!;
+    public long UserId { get; set; }
+    public string VideoPath { get; set; } = null!;
+
+    
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; } = null!;
+}
